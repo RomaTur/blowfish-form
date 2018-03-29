b05ad6f3a9bd3796ab2c88e1a9f30522f81e573d
Roman Turusov <borka.grr@gmail.com>
2018-03-29 17:38:06 +0300

    compiling electron and web app

--- electron_process.js
+++ electron_process.js
@@ -3,10 +3,11 @@ const electron = require('electron')
 const {app, BrowserWindow} = electron
 
 // Let electron reloads by itself when webpack watches changes in ./src/
+if(process.env.ELECTRON_START_URL){
 require('electron-reload')(__dirname)
+}
 
 const path = require('path');
-
 const url = require('url');
 // To avoid being garbage collected
 let mainWindow
@@ -14,12 +15,11 @@ let mainWindow
 app.on('ready', () => {
 
     mainWindow = new BrowserWindow({width: 900, height: 600})
-    // const startUrl = process.env.ELECTRON_START_URL || url.format({
-    //     pathname: path.join(__dirname, './build/index.html'),
-    //     protocol: 'file:',
-    //     slashes: true
-    // });
-    const startUrl = process.env.ELECTRON_START_URL || `file://${__dirname}/build/index.html`;
+    const startUrl = process.env.ELECTRON_START_URL || url.format({
+        pathname: path.join(__dirname, './index.html'),
+        protocol: 'file:',
+        slashes: true
+    });
     mainWindow.loadURL(startUrl);
 
 })
